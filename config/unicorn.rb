98645b9562817878a988fceda4b844090c07e942
kawasaki-atsushi <kawasaki8910@gmail.com>
2015-09-26 16:00:14 +0900

    ターボリンク削除

---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# ワーカーの数
+worker_processes 2
+application = 'chrome_api'
+
+# ソケット
+listen  "/tmp/unicorn_#{application}.sock"
+pid     "/tmp/unicorn_#{application}.pid"
+
+# ログ
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+# ダウンタイムをなくす
+preload_app true
+
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      # SIGTTOU だと worker_processes が多いときおかしい気がする
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
